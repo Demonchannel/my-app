c96bf6d943eae77c46a1343b00e550204e5ca01c
NadyaKuts <nadiakuz97@gmail.com>
2022-10-06 21:26:29 +0300

    fix routes, add styled

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import Explore from "../Pages/Explore";
-import s from "../Styles/Layout.module.css"
+import React from 'react'
+import Explore from '../Pages/Explore'
+import s from '../Styles/Layout.module.css'
 import { Link, Outlet } from 'react-router-dom'
-import Header from "./Header";
-import Menu from "./Menu";
+import Header from './Header'
+import Menu from './Menu'
+import styled from 'styled-components'
 
-const Layout = () =>{
-    return( 
-        <div>
-        <div><Header/></div>
-        <div><Menu/></div>
-        <main className="">
-        <Outlet/>
-</main>
-    </div>
-    )
+const Layout = () => {
+  return (
+    <Container>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Menu />
+      </div>
+      <main className=''>
+        <Outlet />
+      </main>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  background-color: pink;
+`
+
 export default Layout
